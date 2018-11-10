73fe3678f30c295d033be81dbc3c3791b93c3188
Arseny Akinfiev <arseny.jin@ya.ru>
2018-11-10 14:40:23 +0800

    FIX: add handlers

--- smartcontract/authStorage.sol
+++ smartcontract/authStorage.sol
@@ -13,7 +13,7 @@ contract authStorage{
 
     bytes32[] public ids;
 
-    function register(bytes32 _name, address _wallet) public { 
+    function _innit(bytes32 _name, address _wallet) private { 
         Account storage account = accounts[_name];
         
         // account.id = _id;
@@ -26,8 +26,21 @@ contract authStorage{
         return ids;
     }
 
-    function login(bytes32 _id) view public returns(address){ 
+    function _getWallet(bytes32 _id) view private returns(address){ 
         return (accounts[_id].wallet);
+    }
+    
+    function signUp(bytes32 _name, address _wallet) public {
+        require(_getWallet(_name) == 0x0000000000000000000000000000000000000000,
+                "ERROR: User name already exists");
+        _innit(_name, _wallet);
+    }
+ 
+    function signIn(bytes32 _id) public returns(address) {
+        require(_getWallet(_id) != 0x0000000000000000000000000000000000000000,
+                "ERROR: User does not exist");
+        address walletAddress = _getWallet(_id);
+        return walletAddress;
     }
  
 }
