8769ec2575924e092fb29feb07bb8e959d311691
Joy <joy.js.mun@gmail.com>
2018-11-10 18:58:40 +0800

    WIP:axios

--- front-end/src/ components/pages/Register.js
+++ front-end/src/ components/pages/Register.js
@@ -3,11 +3,51 @@ import {Link} from 'react-router-dom';
 import './styles.scss';
 
 class Register extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+           username:''
+           
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange (e) {
+
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+
+    handleSubmit(e) {
+       
+        e.preventDefault();
+        console.log(this.state.username);
+        const cachedHits = localStorage.setItem("username", this.state.username); // maybe we will change it later
+        console.log(localStorage);
+        if (cachedHits) {
+            this.setState({ username: JSON.parse(cachedHits) });
+            console.log("local storage :" + cachedHits );
+            return;
+          }
+        this.props.history.push('/register_2');
+
+      
+    
+        
+        
+
+    }
     render() {
         return (
             <div>
                 <div className="container auth">
               <Link className="logo" to="/">Heike</Link>
+              <form onSubmit={this.handleSubmit} >
               <div className="card">
                   <div className="header blue white-text center">
                       <div className="card-content">Register</div>
@@ -17,17 +57,24 @@ class Register extends Component {
                 <div className="row">
                 <div>
                 <div className="input-field col s12 username">
-                    <label>Username</label>
+                    
                     <input
                     name="username"
                     type="text"
-                    className="validate"/>
+                    className="validate"
+                    placeholder="Enter Your Username"
+                    onChange={this.handleChange} 
+                    value={this.state.username}/>
+                     
                 </div>
             </div>
-                    <Link to='/register_2' className="waves-effect waves-light btn">CREATE</Link> 
+               
+                    <button type="submit" className="waves-effect waves-light btn">CREATE</button> 
+              
                 </div>
             </div>
               </div>
+              </form>
           </div>
             </div>
         );
